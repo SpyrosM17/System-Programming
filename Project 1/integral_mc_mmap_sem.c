ffa2382fdd70c441a97cdb8b8cda8cb480741996
Spyros Mantadakis <165203045+SpyrosM17@users.noreply.github.com>
2025-10-17 21:27:51 +0300

    untested code για τα υπολοιπα

--- Project 1/integral_mc_mmap_sem.c
+++ Project 1/integral_mc_mmap_sem.c
@@ -1,5 +1,96 @@
-// TODO: use shared memory (mmap) and semaphore
+// Panagiotis Hadijdoukas, 3456789
+// Giorgos Papadimitrou Ilias, 1234567
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <semaphore.h>
+
+double get_wtime(void) {
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    return (double)t.tv_sec + (double)t.tv_usec * 1.0e-6;
+}
+
+inline double f(double x) {
+    return sin(cos(x));
+}
 
 int main(int argc, char *argv[]) {
-  return 0;
+    double a = 0.0;
+    double b = 1.0;
+    unsigned long n = 24e7;
+    const double h = (b-a)/n;
+    const double ref = 0.73864299803689018;
+    int nprocs = 4;
+    
+    if (argc >= 2) {
+        n = atol(argv[1]);
+    }
+    if (argc >= 3) {
+        nprocs = atoi(argv[2]);
+    }
+
+    double t0 = get_wtime();
+    
+    // Create shared memory for result and semaphore
+    typedef struct {
+        double total_result;
+        sem_t semaphore;
+    } shared_data_t;
+    
+    shared_data_t *shared_data = mmap(NULL, sizeof(shared_data_t), 
+                                     PROT_READ | PROT_WRITE, 
+                                     MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    
+    shared_data->total_result = 0.0;
+    sem_init(&shared_data->semaphore, 1, 1); // 1 = shared between processes
+    
+    unsigned long iter_per_proc = n / nprocs;
+    
+    for (int i = 0; i < nprocs; i++) {
+        pid_t pid = fork();
+        
+        if (pid == 0) { // Child process
+            unsigned long start_iter = i * iter_per_proc;
+            unsigned long end_iter = (i == nprocs-1) ? n : start_iter + iter_per_proc;
+            unsigned long local_iter = end_iter - start_iter;
+            
+            srand48(getpid() + i);
+            
+            double local_res = 0.0;
+            for (unsigned long j = 0; j < local_iter; j++) {
+                double xi = drand48();
+                local_res += f(xi);
+            }
+            
+            // Critical section - update shared result
+            sem_wait(&shared_data->semaphore);
+            shared_data->total_result += local_res;
+            sem_post(&shared_data->semaphore);
+            
+            exit(0);
+        }
+    }
+    
+    // Wait for all children
+    for (int i = 0; i < nprocs; i++) {
+        wait(NULL);
+    }
+    
+    double total_res = shared_data->total_result * h;
+    double t1 = get_wtime();
+    
+    // Cleanup
+    sem_destroy(&shared_data->semaphore);
+    munmap(shared_data, sizeof(shared_data_t));
+    
+    printf("Result=%.16f Error=%e Rel.Error=%e Time=%lf seconds Processes=%d\n",
+           total_res, fabs(total_res-ref), fabs(total_res-ref)/ref, t1-t0, nprocs);
+    
+    return 0;
 }
--- Project 1/integral_mc_mq.c
+++ Project 1/integral_mc_mq.c
@@ -1,5 +1,96 @@
-// TODO: message queue
+// Panagiotis Hadijdoukas, 3456789
+// Giorgos Papadimitrou Ilias, 1234567
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <mqueue.h>
+#include <fcntl.h>
+
+double get_wtime(void) {
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    return (double)t.tv_sec + (double)t.tv_usec * 1.0e-6;
+}
+
+inline double f(double x) {
+    return sin(cos(x));
+}
 
 int main(int argc, char *argv[]) {
-  return 0;
+    double a = 0.0;
+    double b = 1.0;
+    unsigned long n = 24e7;
+    const double h = (b-a)/n;
+    const double ref = 0.73864299803689018;
+    int nprocs = 4;
+    
+    if (argc >= 2) {
+        n = atol(argv[1]);
+    }
+    if (argc >= 3) {
+        nprocs = atoi(argv[2]);
+    }
+
+    double t0 = get_wtime();
+    
+    // Create message queue
+    struct mq_attr attr;
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = sizeof(double);
+    attr.mq_curmsgs = 0;
+    
+    mqd_t mq = mq_open("/integral_mq", O_CREAT | O_RDWR, 0666, &attr);
+    
+    unsigned long iter_per_proc = n / nprocs;
+    
+    for (int i = 0; i < nprocs; i++) {
+        pid_t pid = fork();
+        
+        if (pid == 0) { // Child process
+            unsigned long start_iter = i * iter_per_proc;
+            unsigned long end_iter = (i == nprocs-1) ? n : start_iter + iter_per_proc;
+            unsigned long local_iter = end_iter - start_iter;
+            
+            srand48(getpid() + i);
+            
+            double local_res = 0.0;
+            for (unsigned long j = 0; j < local_iter; j++) {
+                double xi = drand48();
+                local_res += f(xi);
+            }
+            
+            // Send result via message queue
+            mq_send(mq, (char*)&local_res, sizeof(local_res), 0);
+            exit(0);
+        }
+    }
+    
+    // Parent process - receive results from message queue
+    double total_res = 0.0;
+    for (int i = 0; i < nprocs; i++) {
+        double partial_res;
+        mq_receive(mq, (char*)&partial_res, sizeof(partial_res), NULL);
+        total_res += partial_res;
+    }
+    
+    total_res *= h;
+    double t1 = get_wtime();
+    
+    // Wait for all children and cleanup
+    for (int i = 0; i < nprocs; i++) {
+        wait(NULL);
+    }
+    
+    mq_close(mq);
+    mq_unlink("/integral_mq");
+    
+    printf("Result=%.16f Error=%e Rel.Error=%e Time=%lf seconds Processes=%d\n",
+           total_res, fabs(total_res-ref), fabs(total_res-ref)/ref, t1-t0, nprocs);
+    
+    return 0;
 }
